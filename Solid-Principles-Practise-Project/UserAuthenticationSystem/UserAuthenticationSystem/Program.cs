ca3aa5196f85b99968f531bad115f18ca5a251d0
Evelyn Foy <evelyn.foy@hotmail.com>
2026-05-30 18:59:47 +0100

    Created a C# console project using Visual Studio with starting code.

---
+++ Solid-Principles-Practise-Project/UserAuthenticationSystem/UserAuthenticationSystem/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace UserAuthenticationSystem;
+
+class Program
+{
+    static void Main()
+    {
+        UserManager manager = new UserManager();
+
+        manager.RegisterUser(
+            "john123",
+            "password123",
+            "john@test.com");
+    }
+}
---
+++ Solid-Principles-Practise-Project/UserAuthenticationSystem/UserAuthenticationSystem/UserManager.cs
@@ -0,0 +1,50 @@
+﻿
+namespace UserAuthenticationSystem;
+
+public class UserManager
+{
+    public void RegisterUser(
+        string username,
+        string password,
+        string email)
+    {
+        // Validationnip
+        if (username.Length < 3)
+        {
+            Console.WriteLine("Username too short");
+            return;
+        }
+
+        if (!email.Contains("@"))
+        {
+            Console.WriteLine("Invalid email");
+            return;
+        }
+
+        // Password hashing
+        string hashedPassword = "HASHED_" + password;
+
+        // Save to database
+        File.AppendAllText(
+            "users.txt",
+            $"{username},{hashedPassword},{email}\n");
+
+        // Send welcome email
+        Console.WriteLine($"Sending welcome email to {email}");
+
+        // Analytics logging
+        Console.WriteLine($"Analytics: New user registered {username}");
+
+        // Audit logging
+        Console.WriteLine($"Audit Log: {username} registered");
+
+        // Auto login
+        Console.WriteLine($"{username} logged in");
+
+        // Generate JWT token
+        string token = Guid.NewGuid().ToString();
+
+        Console.WriteLine($"JWT Token: {token}");
+    }
+}
+
